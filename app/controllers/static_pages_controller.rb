ed04e6371c8baf1269773bbd386aaa3c0dab3394
Pablo Ochoa <pabloivan57@gmail.com>
2013-12-12 17:50:53 -0500

    changes on ga code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,27 +4,31 @@ require 'uri/http'
 class StaticPagesController < ApplicationController
 
   def home
+    puts request.env['HTTP_REFERER']
   end
 
   def fake
     fetch("http://staging.seo-platform.haystak.com/")
 
-    url = URI.parse("http://staging.seo-platform.haystak.com/")
-    req = Net::HTTP::Get.new(url.path)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    puts res.body
+    #url = URI.parse("http://staging.seo-platform.haystak.com/")
+    #req = Net::HTTP::Get.new(url.path, {
+    #    'Referer' => "http://www.haystak.com?q='haystack'",
+    #    'User-Agent' => "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)"
+    #})
+    #res = Net::HTTP.start(url.host, url.port) {|http|
+    #  http.request(req)
+    #}
     render 'home'
   end
 
   private
   def fetch(uri_str, limit = 10)
-    # You should choose better exception.
     raise ArgumentError, 'HTTP redirect too deep' if limit == 0
 
     url = URI.parse(uri_str)
-    req = Net::HTTP::Get.new(url.path)
+    req = Net::HTTP::Get.new(url.path, {
+        'Referer' => "http://www.haystak-search.com?q='haystack'"
+    })
     response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
     puts response.inspect
     case response
