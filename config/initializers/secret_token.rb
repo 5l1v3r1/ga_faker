0e38ea6436c996f779836ee9fcc43707d53c75f3
Pablo Ochoa <pabloivan57@gmail.com>
2013-12-12 12:22:17 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GaFaker::Application.config.secret_key_base = 'ea7aeb0964bb966025b94e7e76220d87df860ce5c5a6ff761bee81c7eb0e80cc00603c640dade52d8e5cb400159ef19b86e023c49f3ebb8b74dd9a8f9d4a1ed3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GaFaker::Application.config.session_store :cookie_store, key: '_ga_faker_session'
